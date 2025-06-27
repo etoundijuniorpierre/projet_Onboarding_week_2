a7cabfd5240cf43f2af78a522265577822da937c
etoundijuniorpierre <etoundijunior@hotmail.com>
2025-06-27 10:41:01 +0100

    fix : clean main code

--- location-service/src/main/java/com/location_service/entity/LocationEntity.java
+++ location-service/src/main/java/com/location_service/entity/LocationEntity.java
@@ -3,9 +3,6 @@ package com.location_service.entity;
 
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
